3e59af82c04d953ebc2a03e6a12324e501e295b6
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-10-07 15:53:34 +0000

    Set up allauth

--- custom_cupboards/urls.py
+++ custom_cupboards/urls.py
@@ -1,5 +1,4 @@
-"""custom_cupboards URL Configuration
-
+"""custom_cutting URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -14,8 +13,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
