bcff0bcc3d4e3fee526dee4f3ddf32527e4a9028
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-10-11 12:41:41 +0000

    Added designs view and templates

--- cupboards/views.py
+++ cupboards/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Design
 
+# Create your views here.
+
+
+def all_designs(request):
+    """ A view to show all designs, including sorting and search queries """
+
+    designs = Design.objects.all()
+
+    context = {
+        'designs': designs,
+    }
+
+    return render(request, 'designs/designs.html', context) 
 # Create your views here.
