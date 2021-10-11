8555c8c63de937413fe3e825608707a4b3d0c3ef
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-10-11 11:14:14 +0000

    customised admin

--- cupboards/admin.py
+++ cupboards/admin.py
@@ -3,6 +3,29 @@ from .models import Material, Design
 
 # Register your models here.
 
-admin.site.register(Material)
-admin.site.register(Design)
-# Register your models here.
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'material_id',
+        'name',
+        'price_per_m2',
+    )
+
+    ordering = ('display_name',)
+
+
+class DesignAdmin(admin.ModelAdmin):
+    list_display = (
+        'design_id',
+        'name',
+        'type',
+        'design_surcharge',
+        'example_price',
+        'main_image'
+    )
+
+    ordering = ('design_id',)
+
+
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Design, DesignAdmin)
