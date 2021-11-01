faae331ff515792495eeec75009dcdeb5297f094
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-11-01 21:52:15 +0000

    Added content and stripe elements to checkout template and associated css and js

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JY91EGe4ow3O78WoZ8vB4oxMAI2WfjAtzS9xbCXXXhMxPxAgea3xYDwMQabMtsFtMHdbFAqLieOd7SX20N9L7N700CHUczCPa',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
