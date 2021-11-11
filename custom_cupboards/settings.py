529dc9a6eaeaafc26ba233c4d919e1ceff2c1c58
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-11-11 20:31:57 +0000

    Created custom_storages.py and added classes for static and media files.  Updated settings.py to find and look for AWS storage bucket for these files when on Heroku

--- custom_cupboards/settings.py
+++ custom_cupboards/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -177,6 +178,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'custom-cupboards'
+    AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # stripe
 STRIPE_CURRENCY = 'gbp'
