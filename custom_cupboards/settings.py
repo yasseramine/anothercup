59197f0e2eb0c34b840cde7195eaf38c8dc94441
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-11-11 20:55:16 +0000

    'Corrected' bucket name in settings.py to contain spelling mistake in actual bucket name

--- custom_cupboards/settings.py
+++ custom_cupboards/settings.py
@@ -181,7 +181,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'custom-cupboards'
+    AWS_STORAGE_BUCKET_NAME = 'custom-cubpboards'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
