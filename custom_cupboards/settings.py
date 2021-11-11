a70e3382bf07212dc7010652898b1c9c46e2c6cb
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-11-11 00:50:08 +0000

    Removed secret key and set debug

--- custom_cupboards/settings.py
+++ custom_cupboards/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+&mmou^hj@z4qm#c+hinc-=wn_ovl#h0yrcn)r$yjo2_z)d)*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['custom-cupboards.herokuapp.com', 'localhost']
 
